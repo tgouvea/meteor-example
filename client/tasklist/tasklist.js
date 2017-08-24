e870baf87a24abc12bd8d7d31ca8762e0a1fbbb8
Tiago Gouvêa <tr617080@oi.corp.net>
2017-08-24 13:43:48 -0300

    First commit

---
+++ client/tasklist/tasklist.js
@@ -0,0 +1,17 @@
+Template.tasklist.helpers({
+
+    task : function(){
+
+        return [
+
+            {name: "Task 1"},
+            {name: "Task 2"},
+            {name: "Task 3"},
+            {name: "Task 4"}
+
+        ]
+
+    }
+
+
+})
